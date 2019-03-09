323d9565ba61d9a8073f43445f104dad7fddf63b
zhaokai <820641374@qq.com>
2019-03-10 00:13:04 +0800

    add

--- front_end/src/main.js
+++ front_end/src/main.js
@@ -3,52 +3,40 @@ import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
-//import './assets/theme/theme-green/index.css'
 import VueRouter from 'vue-router'
 import store from './vuex/store'
 import Vuex from 'vuex'
-//import NProgress from 'nprogress'
-//import 'nprogress/nprogress.css'
 import routes from './routes'
-import Mock from './mock'
-Mock.bootstrap();
+import axios from 'axios';
 import 'font-awesome/css/font-awesome.min.css'
 
 Vue.use(ElementUI)
 Vue.use(VueRouter)
 Vue.use(Vuex)
 
-//NProgress.configure({ showSpinner: false });
-
+Vue.prototype.$Host = 'http://localhost:3000/api'
+Vue.prototype.$axios = 'http://localhost:3000/api'
 const router = new VueRouter({
   routes
 })
 
+//全局路由导航
 router.beforeEach((to, from, next) => {
-  //NProgress.start();
-  if (to.path == '/login') {
-    sessionStorage.removeItem('user');
-  }
-  let user = JSON.parse(sessionStorage.getItem('USER-INFO'));
-  // if(user.role && user.role == 1){
+  next(0)
+  // if (to.path == '/login') {
+  //   sessionStorage.removeItem('user');
   // }
-  if (!user && to.path != '/login') {
-    next({ path: '/login' })
-  } else {
-    next()
-  }
+  // let user = JSON.parse(sessionStorage.getItem('USER-INFO'));
+  // if (!user && to.path != '/login') {
+  //   next({ path: '/login' })
+  // } else {
+  //   next()
+  // }
 })
 
-//router.afterEach(transition => {
-//NProgress.done();
-//});
-
 new Vue({
-  //el: '#app',
-  //template: '<App/>',
   router,
   store,
-  //components: { App }
   render: h => h(App)
 }).$mount('#app')
 
